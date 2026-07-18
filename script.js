1ba9975f678b5f059b2b17b4df5519a91f2a1e32
arunt2005 <arunt2005@gmail.com>
2026-07-19 00:59:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,256 @@
+
+
+let originalQuestions = [
+  {
+    "q": "John Cadbury opened his first shop in ________.",
+    "options": [
+      "London",
+      "Manchester",
+      "Birmingham",
+      "Liverpool"
+    ],
+    "answer": "Birmingham"
+  },
+  {
+    "q": "John Cadbury opened his first shop in the year ________.",
+    "options": [
+      "1822",
+      "1824",
+      "1831",
+      "1842"
+    ],
+    "answer": "1824"
+  },
+  {
+    "q": "John Cadbury was ______ years old when he started his business.",
+    "options": [
+      "20",
+      "21",
+      "22",
+      "25"
+    ],
+    "answer": "22"
+  },
+  {
+    "q": "John Cadbury belonged to the ________ community.",
+    "options": [
+      "Roman Catholic",
+      "Hindu",
+      "Quaker",
+      "Muslim"
+    ],
+    "answer": "Quaker"
+  },
+  {
+    "q": "John Cadbury first sold all of the following EXCEPT ________.",
+    "options": [
+      "Tea",
+      "Coffee",
+      "Cocoa",
+      "Biscuits"
+    ],
+    "answer": "Biscuits"
+  },
+  {
+    "q": "John Cadbury prepared cocoa using a ________.",
+    "options": [
+      "Grinder",
+      "Mixer",
+      "Mortar and pestle",
+      "Rolling stone"
+    ],
+    "answer": "Mortar and pestle"
+  },
+  {
+    "q": "John Cadbury advertised his products in the ________.",
+    "options": [
+      "The Times",
+      "Birmingham Gazette",
+      "Daily News",
+      "London Journal"
+    ],
+    "answer": "Birmingham Gazette"
+  },
+  {
+    "q": "John Cadbury started a small factory in ________.",
+    "options": [
+      "1824",
+      "1831",
+      "1842",
+      "1861"
+    ],
+    "answer": "1831"
+  },
+  {
+    "q": "The small factory was located at ________.",
+    "options": [
+      "Green Street",
+      "King Road",
+      "Crooked Lane",
+      "Victoria Road"
+    ],
+    "answer": "Crooked Lane"
+  },
+  {
+    "q": "By 1842, John Cadbury sold ________ varieties of cocoa and chocolate products.",
+    "options": [
+      "10",
+      "12",
+      "14",
+      "16"
+    ],
+    "answer": "16"
+  },
+  {
+    "q": "The cocoa import duty was reduced by ________.",
+    "options": [
+      "King George",
+      "Queen Victoria",
+      "William Gladstone",
+      "George Cadbury"
+    ],
+    "answer": "William Gladstone"
+  },
+  {
+    "q": "Reducing the cocoa tax made chocolate ________.",
+    "options": [
+      "More expensive",
+      "Hard to buy",
+      "More affordable",
+      "Less popular"
+    ],
+    "answer": "More affordable"
+  },
+  {
+    "q": "The first chocolate bar was made by adding ________.",
+    "options": [
+      "Milk",
+      "Sugar",
+      "Cocoa butter",
+      "Honey"
+    ],
+    "answer": "Cocoa butter"
+  },
+  {
+    "q": "Who joined John Cadbury in the family business?",
+    "options": [
+      "William and James",
+      "Richard and George",
+      "Henry and Charles",
+      "Peter and Thomas"
+    ],
+    "answer": "Richard and George"
+  },
+  {
+    "q": "George Cadbury created a milk chocolate bar in ________.",
+    "options": [
+      "1842",
+      "1861",
+      "1904",
+      "1920"
+    ],
+    "answer": "1904"
+  },
+  {
+    "q": "The chocolate made with extra milk was named ________.",
+    "options": [
+      "Highland Milk",
+      "Dairy Maid",
+      "Jersey",
+      "Dairy Milk"
+    ],
+    "answer": "Dairy Milk"
+  },
+  {
+    "q": "Who suggested the name 'Dairy Milk'?",
+    "options": [
+      "John Cadbury",
+      "George Cadbury",
+      "A customer",
+      "William Gladstone"
+    ],
+    "answer": "A customer"
+  },
+  {
+    "q": "By the early 1920s, ________ had become the leading chocolate in the UK.",
+    "options": [
+      "Highland Milk",
+      "Jersey",
+      "Dairy Maid",
+      "Dairy Milk"
+    ],
+    "answer": "Dairy Milk"
+  },
+  {
+    "q": "Decorative chocolate boxes were mainly used as ________.",
+    "options": [
+      "Lunch boxes",
+      "Storage boxes",
+      "Gift boxes",
+      "Toy boxes"
+    ],
+    "answer": "Gift boxes"
+  },
+  {
+    "q": "Why did John Cadbury encourage people to drink cocoa?",
+    "options": [
+      "It was cheap.",
+      "It was colourful.",
+      "It was a healthy alternative to alcohol.",
+      "It was imported."
+    ],
+    "answer": "It was a healthy alternative to alcohol."
+  },
+  {
+    "q": "Why was chocolate considered a luxury in the early days?",
+    "options": [
+      "It was difficult to make.",
+      "It spoiled quickly.",
+      "Cocoa beans were imported and expensive.",
+      "It was available only in winter."
+    ],
+    "answer": "Cocoa beans were imported and expensive."
+  },
+  {
+    "q": "Which quality helped Cadbury become successful?",
+    "options": [
+      "Colourful advertisements only",
+      "Low prices only",
+      "Quality products and purity",
+      "Selling sweets only"
+    ],
+    "answer": "Quality products and purity"
+  },
+  {
+    "q": "Which event happened FIRST?",
+    "options": [
+      "Dairy Milk became popular.",
+      "George Cadbury created milk chocolate.",
+      "John Cadbury started a factory.",
+      "John Cadbury opened his first shop."
+    ],
+    "answer": "John Cadbury opened his first shop."
+  },
+  {
+    "q": "Which of the following is NOT a name suggested for the new milk chocolate?",
+    "options": [
+      "Highland Milk",
+      "Jersey",
+      "Dairy Maid",
+      "Chocolate King"
+    ],
+    "answer": "Chocolate King"
+  },
+  {
+    "q": "What is the main idea of this lesson?",
+    "options": [
+      "How to make chocolates at home.",
+      "The history of tea and coffee.",
+      "The journey of John Cadbury and the growth of the Cadbury chocolate business.",
+      "How to advertise newspapers."
+    ],
+    "answer": "The journey of John Cadbury and the growth of the Cadbury chocolate business."
+  }
+];
+
+
